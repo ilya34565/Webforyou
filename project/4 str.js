5038b33fd9318a0c10bbc096ea37d2d80c885ac1
ilya34565 <ilya.konkov.1997@mail.ru>
2022-06-10 18:25:42 +0300

    2

---
+++ project/4 str.js
@@ -0,0 +1,44 @@
+alert("Привет, я JS");
+
+
+
+
+function gameGuees() {
+var gueesNum = 5;
+var ourNum = prompt("Введите число:");
+    if (ourNum > gueesNum) {
+        alert("Число должно быть меньше!");
+        return gameGuees();
+    }else if (ourNum < gueesNum) {
+        alert("Число должно быть больше!");
+        return gameGuees();
+    } else {
+        return alert("Вы угадали!");
+    }
+}
+
+//gameGuees();
+
+function reminder() {
+    alert("СЛИШКОМ ДОЛГО");
+    //windiw.close();
+}
+
+//setTimeout(reminder, 5000);
+
+function valide() {
+    var name = document.getElementDyId("name").value;
+    var password = document.getElementDyId("password").value;
+    
+      var reg_name = /^[a-za-я]+$/i;
+    var pass_name = /[z-z0-9]{5,100}$/i;
+    
+    if (reg_name.test(name) == false) {
+        alert("Неверное ФИО");
+    }
+    
+    if (reg_password.test(password) == false) {
+        alert("Неверный пароль");
+    }
+
+  
